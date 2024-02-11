05471f4e75c71eccd30c0974c371c0be9763d12c
Shafayet <11shafayet@gmail.com>
2024-02-11 16:48:25 +0600

    updated home page

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -1,11 +1,10 @@
 'use client';
 
-import logo from '../../public/assets/logo.png';
-import { FaTimes } from 'react-icons/fa';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import logo from '/public/assets/logo.png';
+import { usePathname } from 'next/navigation';
 
 const navItems = [
   {
@@ -17,117 +16,79 @@ const navItems = [
     link: '/about-us',
   },
   {
-    text: 'Classes',
-    link: '/classes',
+    text: 'Academic',
+    link: '/academic',
+  },
+  {
+    text: 'Department',
+    link: '/department',
   },
   {
     text: 'Notices',
     link: '/notices',
   },
   {
-    text: 'Teachers & Staff',
-    link: 'teachers-and-staff',
+    text: 'Exams',
+    link: '/exams',
   },
   {
     text: 'Admission',
-    link: 'admission',
+    link: '/admission',
   },
   {
-    text: 'Questions',
-    link: 'questions',
+    text: 'Gallery',
+    link: '/gallery',
   },
 ];
 
 const Navbar = () => {
-  const [active, setActive] = useState(false);
-  const [userDash, setUserDash] = useState(false);
-
-  // closing mobile nav if clicked on outside of the box
-  const handleOutsideClick = (event) => {
-    const mobileNav = document.querySelector('.mobileNav');
-    if (mobileNav && !mobileNav.contains(event.target)) {
-      setActive(false);
-    }
-  };
-  useEffect(() => {
-    if (active) {
-      document.addEventListener('click', handleOutsideClick);
-    } else {
-      document.removeEventListener('click', handleOutsideClick);
-    }
-
-    return () => {
-      document.removeEventListener('click', handleOutsideClick);
-    };
-  }, [active]);
+  const [activeNav, setActiveNav] = useState(false);
+  const pathname = usePathname();
 
   return (
-    <div className="relative bg-white shadow-md py-2">
-      {userDash && (
-        <div
-          className="absolute inset-0 h-screen w-screen bg-black bg-opacity-0 z-[100] overflow-hidden"
-          onClick={() => setUserDash(false)}
-        />
-      )}
-      <div className="container mx-auto px-4">
-        <div className="hidden lg:flex justify-between items-center">
-          {/* logo */}
-          <div>
-            <Image
-              src={logo}
-              alt="logo"
-              className="max-w-[120px] h-auto object-cover"
-            />
-          </div>
-
-          {/* nav */}
-          <ul className="flex items-center gap-x-6">
-            {navItems.map((item, i) => (
-              <li className="text-base font-medium hover:text-primary" key={i}>
-                <Link href={item.link}>{item.text}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        {/* mobile navigation */}
-        <div className=" lg:hidden flex justify-between items-center gap-x-4">
-          <div>
-            <Image src={logo} alt="logo" />
-          </div>
-          <div
-            className="cursor-pointer hover:text-primary"
-            onClick={() => setActive((prev) => !prev)}
-          >
-            <GiHamburgerMenu size={30} />
+    <>
+      <div className="relative bg-primary bg-opacity-10 shadow-md py-2">
+        <div className="container mx-auto px-4">
+          <div className="flex gap-4 flex-col md:flex-row items-center">
+            <Image src={logo} alt="logo" height={55} width={55} />
+            <h4 className="flex-grow text-center font-bold text-xl">
+              Beanibazar Govt. College (বিয়ানিবাজার সরকারি কলেজ)
+            </h4>
           </div>
         </div>
       </div>
-
-      <div
-        className={`block lg:hidden mobileNav absolute w-full sm:w-1/2 h-screen right-0 bg-gray-100 z-[11111] duration-500 ${
-          active ? 'translate-x-0' : ' translate-x-full'
-        }`}
-      >
-        <div
-          className="absolute top-5 right-5 cursor-pointer hover:text-primary"
-          onClick={() => setActive(false)}
-        >
-          <FaTimes size={30} />
-        </div>
-        <ul className="flex flex-col items-center gap-y-6 mt-12">
-          {navItems.map((item, i) => (
-            <li
-              className="text-xl font-medium hover:text-primary"
-              key={i}
-              onClick={() => setActive(false)}
+      <div className="relative bg-white shadow-md py-4">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-col gap-y-2 justify-between items-center">
+            <div
+              className="relative w-full flex flex-col gap-y-1.5 justify-center items-center md:hidden border border-black rounded py-2 cursor-pointer"
+              onClick={() => setActiveNav((prev) => !prev)}
             >
-              <Link href={item.link}>{item.text}</Link>
-            </li>
-          ))}
-        </ul>
+              <span className="block w-11 h-0.5 bg-black" />
+              <span className="block w-11 h-0.5 bg-black" />
+              <span className="block w-11 h-0.5 bg-black" />
+            </div>
+            {/* nav */}
+            <ul
+              className={`flex flex-col md:flex-row items-center justify-between w-full gap-6 py-4 md:py-0 ${
+                activeNav ? 'flex' : 'hidden md:flex'
+              }`}
+            >
+              {navItems.map((item, i) => (
+                <li
+                  className={`text-base font-medium hover:text-primary ${
+                    pathname === item.link && 'text-primary'
+                  }`}
+                  key={i}
+                >
+                  <Link href={item.link}>{item.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ components/home/PrincipalBio.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import user from '/public/assets/teachers/user.png';
+
+const PrincipalBio = () => {
+  return (
+    <div className="bg-white shadow-light rounded-md p-4 md:p-6">
+      <div className="flex flex-col sm:flex-row gap-4 items-start">
+        <Image src={user} alt="" className="w-20 h-20 object-cover" />
+        <div>
+          <h5 className="text-xl font-semibold">The Name Of the Principle</h5>
+          <h5 className="text-lg font-medium">Principle</h5>
+          <p className="opacity-75">2013-On Going</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrincipalBio;
